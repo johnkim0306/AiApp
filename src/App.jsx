15a51b4362fdc14c18add07f3e4f8c0ad324e26c
John Kim <mkcokr2176@gmail.com>
2023-03-03 11:02:10 -0800

    Implement translation component & Setting up partially done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import './App.css';
+import { Configuration, OpenAIApi } from "openai";
 import OptionSelection from './components/OptionSelection';
 import { arrayItems } from './AIOption';
+import Translation from './components/Translation';
+import { useState } from 'react';
 
 function App() {
-  const apiKey = process.env.REACT_APP_API_KEY;
-  console.log(apiKey);
+  //apiKey: process.env.OPENAI_API_KEY,
+  const configuration = new Configuration({
+    apiKey: "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"
+  });
+
+  const openai = new OpenAIApi(configuration);
+  const [option, setOption] = useState({}); 
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
   console.log(arrayItems);
   const selectOption = (option) => {
-    console.log(option);
+    setOption(option);
   };
+
+  const doStuff = async () => {
+    let object = { ...option, prompt: input };
+    const response = await openai.createCompletion(object);
+    setResult(response.data.choices[0].text);
+  };
+  console.log(input);
+
   return (
-    <div className='App'>
-      <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+    <div className="App">
+      {Object.values(option).length === 0 ? (
+        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+      ) : (
+        <Translation doStuff={doStuff} setInput={setInput} result={result} />
+      )}
     </div>
   );  
 }
--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const Translation = () => {
+export default function Translation({ doStuff, setInput, result }) {
   return (
-    <div>Translation</div>
-  )
-}
+    <div>
+      <textarea
+        className="text-area"
+        cols={55}
+        rows={10}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <button className="action-btn" onClick={doStuff}>
+        DO YOU STUFF!
+      </button>
 
-export default Translation
+      <h3 className="result-text">{result.length > 0 ? result : ""}</h3>
+    </div>
+  );
+}
